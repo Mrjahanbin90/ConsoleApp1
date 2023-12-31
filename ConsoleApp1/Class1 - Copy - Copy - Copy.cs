c889be90d448cdb3c54170b150d059dd25010c6f
Mohammad Javad Jahanbin <MrJJahanbin@hotmail.com>
2023-12-31 10:56:23 +0330

    ایجاد 3فایل جدید در اصلی

---
+++ ConsoleApp1/Class1 - Copy - Copy - Copy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public void Go()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("gOOD");
+            Console.ReadLine();
+            Console.WriteLine("MIO");
+
+        }
+    }
+}
